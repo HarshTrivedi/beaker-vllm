27b8fd092475a5fcd0f69578d92b6a48fb79815c
Harsh Trivedi <harshjtrivedi94@gmail.com>
2024-01-28 01:37:15 -0500

    minor

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,11 @@ FROM nvcr.io/nvidia/pytorch:23.10-py3
 
 RUN pip install vllm
 
-COPY chat_templates/ .
-COPY entrypoint.py .
+COPY chat_templates/ /workspace/chat_templates/
+COPY entrypoint.py /workspace/entrypoint.py
 
 ENV MODEL_NAME="facebook/opt-125m"
 ENV HF_HOME="~/.cache/huggingface/"
 ENV NUM_GPUS=1
 
-# ENTRYPOINT ["sh", "-c", "HF_HOME=$HF_HOME python -m vllm.entrypoints.openai.api_server --model $MODEL_NAME --tensor-parallel-size $NUM_GPUS --chat-template chat_templates/template_chatml.jinja2"]
-ENTRYPOINT ["sh", "-c", "python entrypoint.py $MODEL_NAME --hf_home $HF_HOME --num_gpus $NUM_GPUS"]
+ENTRYPOINT ["sh", "-c", "python /workspace/entrypoint.py $MODEL_NAME --hf_home $HF_HOME --num_gpus $NUM_GPUS"]
--- entrypoint.py
+++ entrypoint.py
@@ -3,8 +3,9 @@ import os
 import subprocess
 
 
+file_directory = os.path.dirname(os.path.abspath(__file__))
 MODEL_NAME_TO_CHAT_TEMPLATE = {
-    "OpenLemur/lemur-70b-chat-v1": os.path.join("chat_templates", "template_chatml.jinja"),
+    "OpenLemur/lemur-70b-chat-v1": os.path.join(file_directory, "chat_templates", "template_chatml.jinja"),
 }
 
 
@@ -26,13 +27,13 @@ def main() -> None:
     args = parser.parse_args()
 
     command = (
-        f"HF_HOME=${args.hf_home} python -m vllm.entrypoints.openai.api_server "
+        f"HF_HOME={args.hf_home} python -m vllm.entrypoints.openai.api_server "
         f"--model {args.model_name} --tensor-parallel-size {args.num_gpus}"
     )
     chat_template = MODEL_NAME_TO_CHAT_TEMPLATE.get(args.model_name, None)
-    # if chat_template is not None: # Disable temporarily.
-    assert os.path.exists(chat_template), f"Chat template {chat_template} does not exist."
-    command += f" --chat-template {chat_template}"
+    if chat_template is not None: # Disable temporarily.
+        assert os.path.exists(chat_template), f"Chat template {chat_template} does not exist."
+        command += f" --chat-template {chat_template}"
 
     print(f"Running: {command}")
     subprocess.run(command, shell=True)
